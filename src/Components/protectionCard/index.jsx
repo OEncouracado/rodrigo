eaa803183015e8c631a9be2b1f1e81f2f78841dc
OEncouracado <tryironcl4d@gmail.com>
2023-05-10 08:55:03 -0300

    a

--- src/Components/protectionCard/index.jsx
+++ src/Components/protectionCard/index.jsx
@@ -7,7 +7,7 @@ function ProtectionCard({ title, price, descrip, zap, msg , isLightMode }) {
     <div className="protection-card">
       <h3 className="protection-card__title py-2">{title}</h3>
       <p className="protection-card__description">{descrip}</p>
-      <p className="protection-card__price">{price}</p>
+      <p className="protection-card__price">{price}</p> 
       <Button target="_blank" rel="noopener noreferrer" href={zap} className="d-flex justify-content-center mx-4 my-3 protection-card__button btn btn-primary">Quero Contratar</Button>
     </div>
   )
